74902a4c3f5270ce9ccfa74ead70c64a0f60a824
Frank Lemanschik <frank@dspeed.eu>
2017-05-23 09:55:54 +0100

    0.1.5

--- device.js
+++ device.js
@@ -5,13 +5,78 @@ class CCDevice extends EventEmitter {
   constructor(bus, config){
     debug('CCDevice::')('NEW CCDevice');
     super();
-    this.commands = this.commands || {};
-    this.commands.simplePoll = 254;
-    this.commands.addressPoll = 253;
-    this.commands.addressClash = 252;
-    this.commands.addressChange = 251;
-    this.commands.addressRandom = 250;
-
+    this.commands = {
+      simplePoll: 254,
+      addressPoll: 253,
+      addressClash: 252,
+      addressChange: 251,
+      addressRandom: 250,
+      requestPoolingPriority: 249,
+      requestStatus: 248,
+      requestVariableSet: 247,
+      requestManufacturerId: 246,
+      requestEquipmentCategoryId: 245,
+      requestProductCode: 244,
+      requestDatabaseVersion: 243,
+      requestSerialNumber: 242,
+      requestSoftwareRevision: 241,
+      testSolenoids: 240,
+      testOutputLines: 238,
+      readInputLines: 237,
+      readOptoStates: 236,
+      latchOutputLines: 233,
+      performSelfCheck: 232,
+      modifyInhibitStatus: 231,
+      requestInhibitStatus: 230,
+      readBufferedCredit: 229,
+      modifyMasterInhibit: 228,
+      requestMasterInhibitStatus: 227,
+      requestInsertionCounter: 226,
+      requestAcceptCounter: 225,
+      modifySorterOverrideStatus: 222,
+      requestSorterOverrideStatus: 221,
+      requestDataStorageAvailability: 216,
+      requestOptionFlags: 213,
+      requestCoinPosition: 212,
+      modifySorterPath: 210,
+      requestSorterPath: 209,
+      teachModeControl: 202,
+      requestTeachStatus: 201,
+      requestCreationDate: 196,
+      requestLastModificationDate: 195,
+      requestRejectCounter: 194,
+      requestFraudCounter: 193,
+      requestBuildCode: 192,
+      modifyCoinId: 185,
+      requestCoinId: 184,
+      uploadWindowData: 183,
+      downloadCalibrationInfo: 182,
+      requestThermistorReading: 173,
+      requestBaseYear: 170,
+      requestAddressMode: 169;
+      requestCommsRevision: 4,
+      clearCommsStatusVariables: 3,
+      requestCommsStatusVariables: 2,
+      resetDevice: 1
+    };
+    this.eventCodes ={
+      254: 'return',
+      20: 'string',
+      19: 'slow',
+      13: 'busy',
+      8: 'following',
+      2: 'inhibited',
+      1: 'rejected',
+      0: 'accepted',
+      accepted: 0,
+      rejected: 1,
+      inhibited: 2,
+      following: 8,
+      busy: 13,
+      slow: 19,
+      string: 20,
+      'return': 254
+    };
     if(!bus && !config) {
       // initialize prototype only
       return;
